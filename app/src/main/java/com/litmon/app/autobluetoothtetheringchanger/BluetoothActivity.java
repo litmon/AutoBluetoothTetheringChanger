52ef0d9d7ded0e680d07da8536bc686ce8b3fc2d
Fukuo Kadota <app.fk91@gmail.com>
2015-12-23 04:14:23 +0900

    Activity起動時のToast追加

--- app/src/main/java/com/litmon/app/autobluetoothtetheringchanger/BluetoothActivity.java
+++ app/src/main/java/com/litmon/app/autobluetoothtetheringchanger/BluetoothActivity.java
@@ -1,16 +1,20 @@
 package com.litmon.app.autobluetoothtetheringchanger;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 /**
  * Created by fukuo on 西暦15/12/22.
  */
-public class BluetoothActivity extends AppCompatActivity {
+public class BluetoothActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Toast.makeText(this, "アプリを起動しました！", Toast.LENGTH_SHORT).show();
         finish();
     }
 }
